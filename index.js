d92611f592dda1d8e3ba249bcd5d2d86bdd18717
Christine Yoo <christinecho.s@gmail.com>
2021-01-27 22:08:01 -0800

    implemented search funtionality

--- index.js
+++ index.js
@@ -1,34 +1,29 @@
-'use strict';
+"use strict";
 
-const apiKey = '7qHZvcuggLQhXw9TInW9yGYlNOs0tEBNycneaoO0'; 
-const searchURL = 'https://developer.nps.gov/api/v1/activities/parks';
-
+const apiKey = "7qHZvcuggLQhXw9TInW9yGYlNOs0tEBNycneaoO0";
+const searchURL = "https://developer.nps.gov/api/v1/parks";
 
 function formatQueryParams(params) {
-  const queryItems = Object.keys(params)
-    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-  return queryItems.join('&');
+  const queryItems = Object.keys(params).map(
+    (key) => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`
+  );
+  return queryItems.join("&");
 }
 
 function displayResults(responseJson) {
   // if there are previous results, remove them
-  console.log(responseJson);
-  $('#results-list').empty();
-  // iterate through the items array
-  for (let i = 0; i < responseJson.items.length; i++){
-    // for each video object in the items 
-    //array, add a list item to the results 
-    //list with the video title, description,
-    //and thumbnail
-    $('#results-list').append(
-      `<li><h3>${responseJson.items[i].snippet.title}</h3>
-      <p>${responseJson.items[i].snippet.description}</p>
-      <img src='${responseJson.items[i].snippet.thumbnails.default.url}'>
-      </li>`
-    )};
-  //display the results section  
-  $('#results').removeClass('hidden');
-};
+  $("#results-list").empty();
+    for (let i = 0; i < responseJson.data.length; i++) {
+        $("#results-list").append(
+          `<li><h3>${responseJson.data[i].fullName}</h3>
+            <p>${responseJson.data[i].description}</p>
+            <p><a href="${responseJson.data[i].url}" target="_blank">${responseJson.data[i].url}</a></p>
+            </li>`
+        );
+      }
+  //display the results section
+  $("#results").removeClass("hidden");
+}
 
 function getParks(query, maxResults) {
   const params = {
@@ -36,29 +31,27 @@ function getParks(query, maxResults) {
     q: query,
     limit: maxResults,
   };
-  const queryString = formatQueryParams(params)
-  const url = searchURL + '?' + queryString;
-
-  console.log(url);
+  const queryString = formatQueryParams(params);
+  const url = searchURL + "?" + queryString;
 
   fetch(url)
-    .then(response => {
+    .then((response) => {
       if (response.ok) {
         return response.json();
       }
       throw new Error(response.statusText);
     })
-    .then(responseJson => displayResults(responseJson))
-    .catch(err => {
-      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    .then((responseJson) => displayResults(responseJson))
+    .catch((err) => {
+      $("#js-error-message").text(`Something went wrong: ${err.message}`);
     });
 }
 
 function watchForm() {
-  $('form').submit(event => {
+  $("form").submit((event) => {
     event.preventDefault();
-    const searchTerm = $('#js-search-term').val();
-    const maxResults = $('#js-max-results').val();
+    const searchTerm = $("#js-search-term").val();
+    const maxResults = $("#js-max-results").val();
     getParks(searchTerm, maxResults);
   });
 }
